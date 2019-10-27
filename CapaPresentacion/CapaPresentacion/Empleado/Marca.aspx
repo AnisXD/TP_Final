ddf911c0285c41ce4a6b560d024d7e057360312f
Anahir Machado <machado.anahir@hotmail.com>
2019-10-27 00:22:12 -0300

    cambio de rama

--- CapaPresentacion/CapaPresentacion/Empleado/Marca.aspx
+++ CapaPresentacion/CapaPresentacion/Empleado/Marca.aspx
@@ -4,6 +4,37 @@
 <asp:Content ID="Content2" ContentPlaceHolderID="ContenidoColIzEmpleado" runat="server">
 </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="ContenidoColPpalEmpleado" runat="server">
+    <h1>Administrar Marcas</h1>
+
+    <div id="FormMarca" class="Form">
+       
+        <asp:GridView ID="gdvMarcas" runat="server" AllowPaging="True" AllowSorting="True" AutoGenerateColumns="False" DataKeyNames="ID_MARCA" DataSourceID="Marcas" HorizontalAlign="Center">
+            <Columns>
+                <asp:BoundField DataField="ID_MARCA" HeaderText="ID_MARCA" ReadOnly="True" SortExpression="ID_MARCA" />
+                <asp:BoundField DataField="NOMBRE_MARCA" HeaderText="NOMBRE_MARCA" SortExpression="NOMBRE_MARCA" />
+            </Columns>
+        </asp:GridView>
+        <asp:Label ID="lblTotalRegistros" runat="server" Text="Registros Encontrados: "></asp:Label>
+        <asp:SqlDataSource ID="Marcas" runat="server" ConnectionString="<%$ ConnectionStrings:FINAL_LABConnectionString %>" SelectCommand="SELECT [ID_MARCA], [NOMBRE_MARCA] FROM [MARCAS]"></asp:SqlDataSource>
+        <br />
+        <asp:Label ID="lblIdMarca" runat="server" text="Id Marca: " ></asp:Label>
+        &nbsp;
+        <asp:TextBox ID="txtIdMarca" runat="server" ></asp:TextBox>
+        &nbsp;
+        <br />
+        <asp:Label ID="lblNombreMarca" runat="server" Text="Nombre Marca: " ></asp:Label>
+        &nbsp;
+        <asp:TextBox ID="txtNombreMarca" runat="server" ></asp:TextBox>
+        &nbsp;
+        <br />
+        <asp:Button ID="btnAgregar" runat="server" Text="Agregar" CssClass="Boton"/>
+        <asp:Button ID="btnEditar" runat="server" Text="Editar" CssClass="Boton"/>
+        <asp:Button ID="btnEliminar" runat="server" Text="Eliminar" CssClass="Boton"/>
+        <br />
+        
+
+    </div>
+
 </asp:Content>
 <asp:Content ID="Content4" ContentPlaceHolderID="ContenidoColDerEmpleado" runat="server">
 </asp:Content>
--- CapaPresentacion/CapaPresentacion/Empleado/Marca.aspx.designer.cs
+++ CapaPresentacion/CapaPresentacion/Empleado/Marca.aspx.designer.cs
@@ -11,5 +11,95 @@ namespace CapaPresentacion.Empleado {
     
     
     public partial class Marca {
+        
+        /// <summary>
+        /// Control gdvMarcas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gdvMarcas;
+        
+        /// <summary>
+        /// Control lblTotalRegistros.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTotalRegistros;
+        
+        /// <summary>
+        /// Control Marcas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource Marcas;
+        
+        /// <summary>
+        /// Control lblIdMarca.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblIdMarca;
+        
+        /// <summary>
+        /// Control txtIdMarca.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtIdMarca;
+        
+        /// <summary>
+        /// Control lblNombreMarca.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNombreMarca;
+        
+        /// <summary>
+        /// Control txtNombreMarca.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombreMarca;
+        
+        /// <summary>
+        /// Control btnAgregar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAgregar;
+        
+        /// <summary>
+        /// Control btnEditar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnEditar;
+        
+        /// <summary>
+        /// Control btnEliminar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnEliminar;
     }
 }
