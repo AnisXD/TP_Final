e46da0b872d026fa1f48fd0a1714fd84b47dfc9f
AnisXD <machado.anahir@hotmail.com>
2019-11-24 21:27:19 -0300

    revision de clases, cambios en propiedades y domicilio

--- ENTIDADES/Domicilio.cs
+++ ENTIDADES/Domicilio.cs
@@ -7,30 +7,52 @@ using System.Threading.Tasks;
 namespace ENTIDADES
 {
     public class Domicilio
+
     {
-        protected string Altura;
-        protected string Calle;
-        protected Localidad dLocalidad;
-        protected Provincia dProvincia;
+        #region Propiedades
+        protected string _id_provincia;
+        protected string _id_localidad;
+        protected string _calle_y_altura;
+        #endregion
 
-        public Domicilio (String dAltura, String dCalle)
+        #region Constructores
+        public Domicilio()
         {
-            this.Altura = dAltura;
-            this.Calle = dCalle;
-
+            IdProvincia = string.Empty;
+            IdLocalidad = string.Empty;
+            Calle_y_Altura = string.Empty;
         }
 
-        public string dAltura
-        {
-            get { return Altura; }
-            set { Altura = value; }
-        }
 
-        public string dCalle
+        public Domicilio(string Id_Provincia, string Id_Localidad, string Calle_y_Altura)
         {
-            get { return Calle; }
-            set { Calle = value; }
+            this.IdProvincia = Id_Provincia;
+            this.IdLocalidad = Id_Localidad;
+            this.Calle_y_Altura = Calle_y_Altura;
         }
-   
-    }
+        #endregion
+
+        #region Sets y Gets
+
+        public string IdProvincia
+        {
+            get { return _id_provincia; }
+            set { _id_provincia = value; }
+        }
+        public string IdLocalidad
+        {
+            get { return _id_localidad; }
+            set { _id_localidad = value; }
+        }
+        public string Calle_y_Altura
+        {
+            get { return _calle_y_altura; }
+            set { _calle_y_altura = value; }
+        }
+        #endregion
+
+        
+
+       
+}
 }
