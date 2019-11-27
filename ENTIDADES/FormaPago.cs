b3a3b8db0234cc411a621f682c23ab24d7f9abdb
malenabou <malenabou@users.noreply.github.com>
2019-11-27 13:03:05 -0300

    agregue las clases forma de envio, forma de pago, celulares, marcas, provxcel, me faltab ventas yd etalles de ventas y necesito que subas la de fecha que tenes para avanzar

---
+++ ENTIDADES/FormaPago.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTIDADES
+{
+    #region Propiedades
+    class FormaPago
+    {
+            protected string _idformapago;
+            protected string _formapago;
+            protected bool _estado;
+            #endregion
+
+            #region Constructor
+
+            public FormaPago()
+            {
+                this.idformapago = string.Empty;
+                this.formapago = string.Empty;
+                this.estado = true;
+            }
+            public FormaPago(string IDFORMAPAGO, string FORMAPAGO, bool ESTADO)
+            {
+                this.idformapago = IDFORMAPAGO;
+                this.formapago = FORMAPAGO;
+                this.estado = ESTADO;
+            }
+            #endregion
+
+
+            #region Gets y Sets
+            public string idformapago
+            {
+                get { return _idformapago; }
+                set { _idformapago = value; }
+            }
+
+            public string formapago
+            {
+                get { return _formapago; }
+                set { _formapago = value; }
+            }
+
+            public bool estado
+            {
+                get { return _estado; }
+                set { _estado = value; }
+            }
+
+            #endregion
+
+        }
+  
+}
