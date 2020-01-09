7de2c497c18e7482dddcb66d79879d20fbbd69bc
AnisXD <machado.anahir@hotmail.com>
2020-01-09 14:31:20 -0300

    ventas y detalle ventas en empleado

---
+++ CapaPresentacion/CapaPresentacion/Empleado/Ventas.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Empleado/PaginaMaestraEmpleado.master" AutoEventWireup="true" CodeBehind="Ventas.aspx.cs" Inherits="CapaPresentacion.Empleado.Ventas" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadEmpleado" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContenidoColIzEmpleado" runat="server">
+    <asp:Label ID="lblFiltrar" runat="server" Text="Filtrar por: " CssClass="ControlesASP"></asp:Label>
+    <asp:Label ID="lblVendedor" runat="server" Text="Vendedor: " CssClass="ControlesASP"></asp:Label>
+    <asp:TextBox ID="TextBox1" runat="server" CssClass="ControlesASP"></asp:TextBox>
+    <asp:Label ID="lblCliente" runat="server" Text="Cliente: " CssClass="ControlesASP"></asp:Label>
+    <asp:TextBox ID="TextBox2" runat="server" CssClass="ControlesASP"></asp:TextBox>
+    <asp:Label ID="lblAño" runat="server" Text="Año: " CssClass="ControlesASP"></asp:Label>
+    <asp:TextBox ID="txtAño" runat="server" CssClass="ControlesASP"></asp:TextBox>
+    <asp:Label ID="lblMes" runat="server" Text="Mes: " CssClass="ControlesASP"></asp:Label>
+    <asp:DropDownList ID="ddlMes" runat="server" CssClass="ControlesASP">
+        <asp:ListItem>Enero</asp:ListItem>
+        <asp:ListItem>Febrero</asp:ListItem>
+        <asp:ListItem>Marzo</asp:ListItem>
+        <asp:ListItem>Abril</asp:ListItem>
+        <asp:ListItem>Mayo</asp:ListItem>
+        <asp:ListItem>Junio</asp:ListItem>
+        <asp:ListItem>Julio</asp:ListItem>
+        <asp:ListItem>Agosto</asp:ListItem>
+        <asp:ListItem>Septiembre</asp:ListItem>
+        <asp:ListItem>Octubre</asp:ListItem>
+        <asp:ListItem>Noviembre</asp:ListItem>
+        <asp:ListItem>Diciembre</asp:ListItem>
+    </asp:DropDownList>
+    <asp:Button ID="bttnFiltrar" runat="server" Text="Filtrar" CssClass="Boton"/>
+    <asp:Button ID="bttnQuitarFiltro" runat="server" Text="Quitar filtro" CssClass="Boton"/>
+    
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContenidoColPpalEmpleado" runat="server">
+   
+
+    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="ID_VTA" DataSourceID="venta">
+        <Columns>
+            <asp:BoundField DataField="ID_VTA" HeaderText="ID" ReadOnly="True" SortExpression="ID_VTA" />
+            <asp:BoundField DataField="ID_CLI_VTA" HeaderText="CLIENTE" SortExpression="ID_CLI_VTA" />
+            <asp:BoundField DataField="ID_EMPLE_VTA" HeaderText="EMPLEADO" SortExpression="ID_EMPLE_VTA" />
+            <asp:BoundField DataField="FECHA_VTA" HeaderText="FECHA" SortExpression="FECHA_VTA" />
+            <asp:BoundField DataField="ID_FORMAENVIO_VTA" HeaderText="FORMA ENVIO" SortExpression="ID_FORMAENVIO_VTA" />
+            <asp:BoundField DataField="ID_FORMAPAGO_VTA" HeaderText="FORMA PAGO" SortExpression="ID_FORMAPAGO_VTA" />
+            <asp:BoundField DataField="IMPORTE" HeaderText="IMPORTE" SortExpression="IMPORTE" />
+        </Columns>
+    </asp:GridView>
+    <asp:SqlDataSource ID="venta" runat="server" ConnectionString="<%$ ConnectionStrings:FINAL_LABConnectionString %>" SelectCommand="SELECT [ID_VTA], [ID_CLI_VTA], [ID_EMPLE_VTA], [FECHA_VTA], [ID_FORMAENVIO_VTA], [ID_FORMAPAGO_VTA], [IMPORTE] FROM [VENTAS]"></asp:SqlDataSource>
+    <asp:Button ID="bttnNuevaVenta" runat="server" Text="Nueva Venta" CssClass="Boton" />
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="ContenidoColDerEmpleado" runat="server">
+</asp:Content>
---
+++ CapaPresentacion/CapaPresentacion/Empleado/Ventas.aspx.designer.cs
@@ -0,0 +1,141 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace CapaPresentacion.Empleado {
+    
+    
+    public partial class Ventas {
+        
+        /// <summary>
+        /// Control lblFiltrar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblFiltrar;
+        
+        /// <summary>
+        /// Control lblVendedor.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblVendedor;
+        
+        /// <summary>
+        /// Control TextBox1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        
+        /// <summary>
+        /// Control lblCliente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblCliente;
+        
+        /// <summary>
+        /// Control TextBox2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+        
+        /// <summary>
+        /// Control lblAño.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblAño;
+        
+        /// <summary>
+        /// Control txtAño.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtAño;
+        
+        /// <summary>
+        /// Control lblMes.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMes;
+        
+        /// <summary>
+        /// Control ddlMes.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlMes;
+        
+        /// <summary>
+        /// Control bttnFiltrar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button bttnFiltrar;
+        
+        /// <summary>
+        /// Control bttnQuitarFiltro.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button bttnQuitarFiltro;
+        
+        /// <summary>
+        /// Control GridView1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView1;
+        
+        /// <summary>
+        /// Control venta.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource venta;
+        
+        /// <summary>
+        /// Control bttnNuevaVenta.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button bttnNuevaVenta;
+    }
+}
